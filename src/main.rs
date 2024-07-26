64fdf98c95f2e72b4748967bc8ef8e110c75068d
Ēriks Lapiņš <lapinseriks98@gmail.com>
2024-07-26 11:18:08 +0300

    db into folder and tables are files, can update and add key value pairs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use parser::DbBase;
 use serde_json::Value;
 
 mod parser;
@@ -9,22 +10,16 @@ mod prelude {
 }
 
 fn main() {
-    let file = parser::parse("./test.rdb");
-    match file {
-        Ok(contents) => println!("{:?}", contents),
-        Err(e) => println!("{:?}", e),
-    }
-
-    let new_file = parser::DbData::new("rust", HashMap::from([
-        ("New".to_string(), Value::String("Entry".to_string())),
-        ("another".to_string(), Value::String("new entry".to_string())),
-        ("array".to_string(), Value::Array(vec![
-            Value::String("some".to_string()),
-            Value::String("new".to_string()),
-            Value::String("array".to_string())
-        ]))
-    ])).unwrap();
-    let value = new_file.getValue("another").unwrap();
-    println!("{:?}", new_file);
-    println!("{:?}", value);
+    let mut db = DbBase::from_name("testdb".to_string()).unwrap();
+    println!("{:?}", db);
+    // db.add_table("cargo".to_string()).unwrap();
+    let mut table = db.get_table("cargo".to_string()).unwrap();
+    println!("table: {:?}", table);
+    table
+        .update("some".to_string(), Value::Array(Vec::from([Value::String("one".to_string()), Value::String("value".to_string())]))).unwrap();
+        // .get_value("another_key".to_string()).unwrap();
+        // .add("some".to_string(), Value::String("new key".to_string())).unwrap()
+        // .add("another_key".to_string(), Value::Null).unwrap();
+    // println!("value: {:?}", value);
+    println!("table: {:?}", table);
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,46 +1,187 @@
-use std::{collections::HashMap, fs::{self, File}, io::Write};
+use std::{collections::HashMap, fmt::Display, fs::{self, File}, path::Path};
 use serde_json::Value;
 
 use crate::prelude::*;
 
-pub type DbEntry = HashMap<String, Value>;
+pub type DbData = HashMap<String, Value>;
 
 #[derive(Debug)]
-pub struct DbData {
-    path: String,
-    data: DbEntry,
+pub struct DbBase {
+    name: String,
+    tables: Vec<String>,
+}
+
+impl DbBase {
+    pub fn new(name: String) -> Result<Self, String> {
+        //! Creates a new DbBase instance if no such exists, otherwise Errors
+        let base_path = format!("./{name}");
+        let path = Path::new(base_path.as_str());
+        if path.exists() && path.is_dir() {
+            return Err(format!("Database `{name}` already exists"))
+        };
+        fs::create_dir(base_path).expect(format!("Unable to create database `{name}`").as_str());
+
+        Ok(Self {
+            name,
+            tables: Vec::new(),
+        })
+    }
+    pub fn from_name(name: String) -> Result<Self, String> {
+        //! Will create a DbBase instance if such a folder exists, otherwise returns an Error
+        let base_path = format!("./{name}");
+        let path = Path::new(base_path.as_str());
+        if path.exists() && path.is_dir() {
+            let mut tables = Vec::<String>::new();
+            for table in path.read_dir().expect(format!("Unable to read tables in `{name}`").as_str()) {
+                if let Ok(table) = table {
+                    let table_name = table
+                        .file_name().into_string().unwrap()
+                        .replace(&format!(".{EXTENSION}"), "");
+                    tables.push(table_name);
+                }
+            }
+            return Ok(Self {
+                name,
+                tables,
+            })
+        };
+
+        Err(format!("Database `{name}` does not exist"))
+    }
+    pub fn from_name_or_new(self, name: String) -> Self {
+        //! Will create a DbBase instance if such a folder exists, otherwise creates a new one
+        let base_path = format!("./{name}");
+        let path = Path::new(base_path.as_str());
+        if path.exists() && path.is_dir() {
+            let mut tables = Vec::<String>::new();
+            for table in path.read_dir().expect(format!("Unable to read tables in `{name}`").as_str()) {
+                if let Ok(table) = table {
+                    let table_name = table
+                        .file_name().into_string().unwrap()
+                        .replace(&format!(".{EXTENSION}"), "");
+                    tables.push(table_name)
+                }
+            }
+            return Self {
+                name,
+                tables,
+            }
+        };
+
+        fs::create_dir(base_path).expect(format!("Unable to create database `{name}`").as_str());
+
+        Self {
+            name,
+            tables: Vec::new(),
+        }
+    }
+    pub fn add_table(&mut self, name: String) -> Result<DbTable, String> {
+        let has_table = self.tables.iter().find(|x| *x == &name);
+        if has_table.is_some() {
+            return Err(format!("Table `{name}` already exists"))
+        } else {
+            let table = DbTable::new(name.clone(), &self).unwrap();
+            self.tables.push(name);
+            Ok(table)
+        }
+    }
+    pub fn get_table(&self, name: String) -> Result<DbTable, String> {
+        let has_table = self.tables.iter().find(|x| *x == &name);
+        if has_table.is_some() {
+            let table = DbTable::get_table(name.clone(), self.name.clone()).unwrap();
+            Ok(table)
+        } else {
+            return Err(format!("Table `{name}` does not exist"))
+        }
+    }
+}
+
+impl Display for DbBase {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for table in self.tables.iter() {
+            return write!(f, " - {table}")
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct DbTable {
+    name: String,
+    parent_name: String,
+    data: DbData,
     keys: Vec<String>,
 }
 
-impl DbData {
-    pub fn new(name: &str, data: HashMap<String, Value>) -> Result<Self, String> {
-        let file_path = format!("./{name}.{EXTENSION}");
+impl DbTable {
+    pub fn new(name: String, base: &DbBase) -> Result<Self, String> {
+        let file_path = format!("./{}/{name}.{EXTENSION}", base.name);
         let file = File::create_new(&file_path).unwrap();
-        serde_json::to_writer(file, &data).unwrap();
-        parse(file_path.as_str())
+        let empty_data: DbData = HashMap::new();
+        serde_json::to_writer(file, &empty_data).unwrap();
+        Ok(DbTable::get_table(name, base.name.clone()).unwrap())
     }
-    pub fn getValue(&self, key: &str) -> Result<Value, String> {
-        if self.keys.contains(&key.to_string()) {
-            return Ok(self.data.clone().get(key).unwrap().to_owned())
+    pub fn get_table(name: String, base_name: String) -> Result<Self, String> {
+        let filepath = format!("./{}/{}.{}", base_name, name, EXTENSION);
+        let path = Path::new(&filepath);
+        if path.exists() {
+            let file = fs::read_to_string(&filepath).expect(format!("Unable to get table `{name}`").as_str());
+            let contents: DbData = serde_json::from_str(file.as_str()).expect(format!("Unable to get data from `{name}`").as_str());
+            let keys: Vec<String> = contents.clone().into_keys().collect();
+    
+            Ok(
+                Self {
+                    name,
+                    parent_name: base_name,
+                    data: contents,
+                    keys
+                }
+            )
+        } else {
+            Err(format!("No such table `{name}`"))
         }
-        Err("No such key in DB".to_string())
     }
-}
-
-pub fn parse(filepath: &str) -> Result<DbData, String> {
-    let file_extension = filepath.ends_with(EXTENSION);
-    if file_extension == false {
-        return Err("Wrong extension".to_string())
+    fn get_file_path(&self) -> String {
+        format!("./{}/{}.{EXTENSION}", self.parent_name, self.name)
     }
-    let file = fs::read_to_string(filepath).expect("Unable to get the file");
-    let contents: DbEntry = serde_json::from_str(file.as_str()).expect("Unable to serialize");
-    let keys: Vec<String> = contents.clone().into_keys().collect();
+    fn rewrite(&mut self, data: HashMap<String, Value>) -> Result<&mut Self, String> {
+        let file_path = self.get_file_path();
+        let file = File::options().write(true).open(&file_path).expect(format!("No such file or directory: {file_path}").as_str());
+        serde_json::to_writer(file, &data).expect(format!("Failed to write data to `{}` in db `{}`", self.name, self.parent_name).as_str());
+        let new_data = DbTable::get_table(self.name.clone(), self.parent_name.clone()).unwrap();
+        self.data = new_data.data;
+        self.keys = new_data.keys;
+        Ok(self)
 
-    Ok(
-        DbData {
-            path: filepath.to_string(),
-            data: contents,
-            keys
+    }
+    pub fn get_value(&self, key: String) -> Result<Value, String> {
+        if self.keys.contains(&key) {
+            return Ok(self.data.clone().get(&key).unwrap().to_owned())
         }
-    )
+        Err(format!("No such key in DB `{}`: {key}", self.name))
+    }
+    pub fn add(&mut self, key: String, value: Value) -> Result<&mut Self, String> {
+        let name = self.name.clone();
+        let parent_name = self.parent_name.clone();
+        if self.keys.contains(&key) {
+            Err(format!("key `{key}` already exists in `{name}` in db `{parent_name}`"))
+        } else {
+            self.keys.push(key.clone());
+            self.data.insert(key, value);
+            self.rewrite(self.data.clone()).expect(format!("Error while adding to table `{name}` in db `{parent_name}`").as_str());
+            Ok(self)
+        }
+    }
+    pub fn update(&mut self, key: String, value: Value) -> Result<&mut Self, String> {
+        let name = self.name.clone();
+        let parent_name = self.parent_name.clone();
+        if self.keys.contains(&key) {
+            self.data.remove(&key);
+            self.data.insert(key, value);
+            self.rewrite(self.data.clone()).expect(format!("Error while updating table `{name}` in db `{parent_name}`").as_str());
+            Ok(self)
+        } else {
+            Err(format!("key `{key}` does not exist"))
+        }
+    }
 }
